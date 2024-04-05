e83c9c79bdf6c805f12e37e2612fe9798297470a
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-05 07:31:02 -0700

    working version

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import chainlit as cl
 import google.generativeai as genai
+import PIL 
+
+
 
 model = genai.GenerativeModel('models/gemini-pro-vision')
 
@@ -12,7 +15,13 @@ def on_chat_start():
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = model.generate_content(message.content)
+    for element in message.elements:
+        print(element)
+
+    img = PIL.Image.open(message.elements[0].path)
+
+    response = model.generate_content([message.content,img])
+
     await cl.Message(response.text).send()
 
 
