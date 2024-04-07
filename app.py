0ef2ecf8c2a12394ae3325468ab992bf894c8d44
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-07 08:36:38 -0700

    working version with bucket upload and calling gemini-pro-vision

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import io
 import chainlit as cl
 import google.generativeai as genai
+from google.cloud import storage
 import PIL 
-
+import requests
 
 
 model = genai.GenerativeModel('models/gemini-pro-vision')
@@ -15,14 +17,30 @@ def on_chat_start():
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    for element in message.elements:
-        print(element)
 
-    img = PIL.Image.open(message.elements[0].path)
+    #print("Message:")
+    #print(message)
+    #print("Message Elements:")
+    #print(message.elements)
+    #for element in message.elements:
+    #    print("Element:")
+    #    print(element)
+    # Get the BlobReader
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('chainlit-genai-vision-bucket')
+    blob = bucket.blob(message.elements[0].name)
+
+    blob.upload_from_filename(message.elements[0].path)
+
+    blob_as_string = blob.download_as_string()
+
+    bytes = io.BytesIO(blob_as_string)
+    img = PIL.Image.open(bytes)
+
 
     response = model.generate_content([message.content,img])
 
     await cl.Message(response.text).send()
 
-
-        
+    #await cl.Message("").send()
